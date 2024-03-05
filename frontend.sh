fb556d1154fdda3567c9248c06f79e9d85a610d0
mcrowther <mattycrowther1@hotmail.co.uk>
2024-03-05 11:42:07 +0000

    Removed some old files

--- frontend.sh
+++
@@ -1,2 +0,0 @@
-sudo neo4j stop
-docker-compose up --file local-docker-compose.yml --build --remove-orphans --force-recreate --always-recreate-dep 
--- local.sh
+++
@@ -1,2 +0,0 @@
-sudo neo4j stop
-docker-compose --file local-docker-compose.yml up --build --remove-orphans --force-recreate --always-recreate-dep 
